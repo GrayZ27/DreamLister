5897a99d81a14ae9152a5650f5a2b362388c0ac0
GrayZ27 <i520you.chen@qq.com>
2017-08-17 14:30:54 -0500

    update UIView

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Gray Zhen on 8/17/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
